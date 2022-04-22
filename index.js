80343cca56887f1748d47f6bb0e8188c06e07e24
akhateeb22 <akhateeb175@gmail.com>
2022-04-22 17:05:08 +0100

    Finishing CICD pipeline

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use('/hello', (req, res, next)=>{
 app.use('/bye', (req, res, next) => {
     res.send('Bye Bye!');
 });
-console.log('sddfsssssdfsdfsdfasdsadsdf');
+console.log('sddfssddsssdfsdfsdfasdsadsdf');
 
 app.listen(8001, (err)=>{
     if(err) console.log(err);

b3099de9f304004ae3bc07b7832a20ada90b2da0
akhateeb22 <akhateeb175@gmail.com>
2022-04-22 09:55:25 +0100

    node-js sample

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+
+app.use(bodyParser.json());
+
+app.use('/hello', (req, res, next)=>{
+    res.send('Hello there!');
+})
+
+app.listen(3000, (err)=>{
+    if(err) console.log(err);
+    else{
+        console.log('App is running!!');
+    }
+})

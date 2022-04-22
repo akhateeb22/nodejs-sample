01b09d4f5bcf8337d7b511d91d434736c71f05e3
akhateeb22 <akhateeb175@gmail.com>
2022-04-22 14:10:12 +0100

    hello first push

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use('/hello', (req, res, next)=>{
 
 app.use('/bye', (req, res, next) => {
     res.send('Bye Bye!');
-})
+});
 app.listen(8001, (err)=>{
     if(err) console.log(err);
     else{
